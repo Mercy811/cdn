4dae992fc020c5dc278536ec8e42fc5536b8496c
Xinyi Ye <xinyi.ye@amplitude.com>
2024-09-04 15:23:57 -0700

    Revert "Update hubspot.js"
    This reverts commit 6ef403f8d1251a6cdcf6592beebdc29d742d94a6.

--- hubspot.js
+++ hubspot.js
@@ -20,6 +20,7 @@ amplitude.add(partnerIdEnrichmentPlugin());
 
 // Set hubspot cookie as a user property
 var hubspotutk = getCookie("hubspotutk");
+console.log("hubspotutk: ", hubspotutk);
 if (hubspotutk) {
     const identifyEvent = new amplitude.Identify();
     identifyEvent.set("hubspotutk", hubspotutk);
